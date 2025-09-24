057f2d2dd9df1761b127e5ac18f51a20fcf2ddb7
Norumai01 <johnnydinhnguyen2001@gmail.com>
2025-09-24 01:02:17 -0400

    Finished Raspberry Pi B, need to fix some bugs.

--- HW2/RasPi_B/README.md
+++ HW2/RasPi_B/README.md
@@ -1,1 +1,58 @@
 # RasPi B
+
+## Prerequisites
+- Raspberry Pi (4+)
+- 3 LEDs
+- 3 220 ohms resistors (any resistor will do)
+
+### Packages
+
+```bash
+# Create a virtual environment to any desired location
+cd /Documents/{your_project_name}
+
+# Make sure to use --system-site-packages flag
+python3 -m venv --system-site-packages {your_env_name}
+source {your_env_name}/bin/activate
+
+# If the package is not installed, install it
+pip3 install gpiozero paho-mqtt 
+```
+
+### Wiring Diagram
+![Wiring Diagram](./Images/Wiring_Diagram.png)
+- Feel free to use any GPIO pins for your LEDs.
+- For our code, we used GPIO 16, 20, 21.
+
+## Running the code
+Change the broker host and port to your broker host and port
+```python
+BROKER_HOST = {your_broker_host}
+BROKER_PORT = {your_broker_port}
+```
+
+Move the code to the raspberry pi if necessary. Then, run the code
+```bash
+python3 pi_b.py
+```
+
+## Testing the code
+Have mosquitto-clients installed on your raspberry pi.
+```bash
+pip3 install mosquitto-clients
+```
+
+Run the code on a terminal and raspberry pi. 
+You should see repeated messages on the terminal of offline status.
+```bash
+# Terminal 1
+python3 pi_b.py
+```
+
+Pull up another terminal and run the following commands
+```bash
+# Terminal 2
+mosquitto_sub -h localhost -t "Status/RaspberryPiA" -m "online" 
+```
+
+
---
+++ HW2/RasPi_B/pi_b.py
@@ -0,0 +1,139 @@
+"""
+Raspberry Pi B - LED Controller
+Controls 3 LEDs based on light status and device status from other Raspberry Pi's
+"""
+
+import paho.mqtt.client as mqtt
+from gpiozero import LED
+import time
+
+BROKER_HOST = "localhost" # Change to your broker's IP address'
+BROKER_PORT = 1883 # Default port
+timeoutSeconds = 60
+CLIENT_ID = "RaspberryPiB"
+
+# Topics
+LIGHT_STATUS_TOPIC = "LightStatus"
+STATUS_A_TOPIC = "Status/RaspberryPiA"
+STATUS_C_TOPIC = "Status/RaspberryPiC"
+
+# Change pins to match your setup
+LED1_PIN = 21
+LED2_PIN = 20
+LED3_PIN = 16
+
+class RaspberryPiB:
+    def __init__(self):
+        # Initialize LEDs
+        self.led1 = LED(LED1_PIN)
+        self.led2 = LED(LED2_PIN)
+        self.led3 = LED(LED3_PIN)
+
+        # Start LEDs off
+        self.led1.off()
+        self.led2.off()
+        self.led3.off()
+
+        # Status tracker
+        self.piA_status = False
+        self.piC_status = False
+        self.last_light_status = None
+
+
+        # MQTT Client Setup
+        self.client = mqtt.Client(CLIENT_ID)
+        self.client.on_connect = self.on_connect
+        self.client.on_message = self.on_message
+
+    def on_connect(self, client, userdata, flags, rc):
+        # Subscribe to topics
+        client.subscribe(LIGHT_STATUS_TOPIC, qos=2)
+        client.subscribe(STATUS_A_TOPIC, qos=2)
+        client.subscribe(STATUS_C_TOPIC, qos=2)
+
+    def on_message(self, client, userdata, msg):
+        # Handle messages and control LEDs
+        try :
+            topic = msg.topic
+            message = msg.payload.decode("utf-8") # Convert bytes to string
+
+            print(f"Received message on topic {topic}: {message}")
+
+            if topic == LIGHT_STATUS_TOPIC:
+                self.handle_light_status(message)
+            elif topic == STATUS_A_TOPIC:
+                self.handle_status_a(message)
+            elif topic == STATUS_C_TOPIC:
+                self.handle_status_c(message)
+        except Exception as e:
+            print(f"Error handling message: {e}")
+
+    def handle_light_status(self, status):
+        # Handle LightStatus messages
+        self.last_light_status = status
+
+        if self.piC_status:
+            if status == "TurnOn":
+                self.led1.on()
+                print("Light is on - LED1 on")
+            elif status == "TurnOff":
+                self.led1.off()
+                print("Light is off - LED1 off")
+        else:
+            print("LightStatus message received but C is offline - LED1 off")
+
+    def handle_status_a(self, status):
+        # Handle Raspberry Pi A messages
+        self.piA_status = status
+
+        if status == "online":
+            self.piA_status = True
+            self.led2.on()
+            print("Raspberry Pi A is online - LED2 on")
+        elif status == "offline":
+            self.piA_status = False
+            self.led2.off()
+            print("Raspberry Pi A is offline - LED2 off")
+
+    def handle_status_c(self, status):
+        # Handle Raspberry Pi C messages
+        self.piC_status = status
+
+        if status == "online":
+            self.piC_status = True
+            self.led3.on()
+            print("Raspberry Pi C is online - LED3 on")
+
+            if self.last_light_status:
+                self.handle_light_status(self.last_light_status)
+            else:
+                self.led1.off()
+                print("No LightStatus message available - LED1 off")
+        elif status == "offline":
+            self.piC_status = False
+            self.led1.off()
+            self.led3.off()
+            print("Raspberry Pi C is offline - LED1 and LED3 off")
+
+    def run(self):
+        try:
+            print("Starting Raspberry Pi B client...")
+            while True:
+                self.client.connect(BROKER_HOST, BROKER_PORT, timeoutSeconds)
+                self.client.loop_start()
+
+                time.sleep(1)
+
+        except KeyboardInterrupt:
+            print("\nShutting down...")
+
+        finally:
+            self.led1.off()
+            self.led2.off()
+            self.led3.off()
+            self.client.loop_stop()
+            self.client.disconnect()
+
+if __name__ == "__main__":
+    rpiB = RaspberryPiB()
+    rpiB.run()
